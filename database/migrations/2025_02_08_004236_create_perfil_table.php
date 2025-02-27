2c87fc5eb92ac93144708d69ba52506a5a989b91
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-02-27 20:05:37 -0300

    update collumn

--- database/migrations/2025_02_08_004236_create_perfil_table.php
+++ database/migrations/2025_02_08_004236_create_perfil_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('perfil', function (Blueprint $table) {
             $table->id();
-            $table->timestamp('nome_perfil');
+            $table->string('nome_perfil');
             $table->timestamps();
         });
     }
