5510a28ac53a499bdd346fbc3632000acc039054
Seu nome <luisfelipearaujopimenta@gmail.com>
2025-02-28 00:53:57 -0300

    carbon

--- app/Http/Util/Payments/ApiMercadoPago.php
+++ app/Http/Util/Payments/ApiMercadoPago.php
@@ -108,7 +108,7 @@ class ApiMercadoPago
                 'payment_method' => $payment->payment_method_id,
                 'id' => $payment->id,
                 'valorFinal' => $payment->transaction_details->total_paid_amount,
-                'dataPagamento' => Carbon::parse($payment->date_approved)->format('d/m/Y H:i:s')
+                'dataPagamento' => Carbon::parse($payment->date_approved)->timestamp
             ];
         } catch (MPApiException $e) {
             $response = $e->getApiResponse();
