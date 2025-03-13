a6b1a4cdd367b844dd92e5807d043024c1469d70
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-03-13 14:06:21 -0300

    update tempo

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 use App\Jobs\ResetPastasCounter;
 
-Schedule::job(new ResetPastasCounter())->everySecond();
+Schedule::job(new ResetPastasCounter())->everyMinute();
