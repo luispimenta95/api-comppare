149765eb14b9ceb8800655e2c892eecf94f74984
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-03-13 12:32:18 -0300

    teste pastas

--- routes/console.php
+++ routes/console.php
@@ -8,10 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('pastas:reset') ->monthlyOn(1, '00:00');
-/*Schedule::command('pastas:reset')->everyFiveMinutes();
-crontab -e
-* * * * * php /caminho/para/o/seu/projeto/artisan schedule:run >> /dev/null 2>&1
-
-
-*/
+//Schedule::command('pastas:reset') ->monthlyOn(1, '00:00');
+Schedule::command('pastas:reset')->everyFiveMinutes();
