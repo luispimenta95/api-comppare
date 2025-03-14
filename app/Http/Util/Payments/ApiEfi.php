6bbb7fc8ef27dfdfcc13ac2b3ef34feb77493538
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-03-14 12:39:43 -0300

    teste assinatura

--- app/Http/Util/Payments/ApiEfi.php
+++ app/Http/Util/Payments/ApiEfi.php
@@ -54,7 +54,7 @@ class ApiEfi
         $params = [
             "id" => 122656
         ];
-        dd($dados['produto']);
+        //dd($dados['produto']);
 
         $body = [
             "items" =>  $dados['produto'],
