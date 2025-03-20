838ee28a811e02ea3ed9cac5212539a5662bfe25
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-03-19 23:17:13 -0300

    Melhorias gerais : Enum e identação

--- app/Http/Controllers/Api/PlanoController.php
+++ app/Http/Controllers/Api/PlanoController.php
@@ -8,51 +8,48 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\Planos;
 use App\Http\Util\Helper;
-
+use App\Enums\HttpCodesEnum;
 
 class PlanoController extends Controller
 {
-    private  array $codes;
     private ApiEfi $apiEfi;
-
-
 
     public function __construct()
     {
-        $this->codes = Helper::getHttpCodes();
         $this->apiEfi = new ApiEfi();
-
     }
 
-    public function index(): object
+    public function index(): JsonResponse
     {
         $planosAtivos = Planos::where('status', 1)->count();
+
         $response = [
-            'codRetorno' => 200,
-            'message' => $this->codes[200],
+            'codRetorno' => HttpCodesEnum::OK->value,
+            'message' => HttpCodesEnum::OK->description(),
             'totalPlanos' => Planos::count(),
             'planosAtivos' => $planosAtivos,
-            'data' => Planos::all()
-
+            'data' => Planos::all(),
         ];
+
         return response()->json($response);
     }
 
-    public function createPlan(Request $request){
-        $campos = ['nome', 'descricao', 'valor', 'quantidadeTags', 'quantidadePastas' ,'online', 'frequenciaCobranca'];
-
+    public function createPlan(Request $request): JsonResponse
+    {
+        $campos = ['nome', 'descricao', 'valor', 'quantidadeTags', 'quantidadePastas', 'online', 'frequenciaCobranca'];
         $campos = Helper::validarRequest($request, $campos);
 
         if ($campos !== true) {
             $response = [
-                'codRetorno' => 400,
-                'message' => $this->codes[-9],
-                'campos' => $campos
+                'codRetorno' => HttpCodesEnum::BadRequest->value,
+                'message' => HttpCodesEnum::MissingRequiredFields->description(),
+                'campos' => $campos,
             ];
             return response()->json($response);
         }
 
-        if($request->online) {
+        $responseApi = null;
+        if ($request->online) {
             $responseApi = json_decode($this->apiEfi->createPlan($request->nome, $request->frequenciaCobranca), true);
         }
 
@@ -63,66 +60,69 @@ class PlanoController extends Controller
             'frequenciaCobranca' => $request->frequenciaCobranca,
             'quantidadeTags' => $request->quantidadeTags,
             'quantidadePastas' => $request->quantidadePastas,
-            'idHost' => $request->online ? $responseApi['data']['plan_id'] : null
+            'idHost' => $request->online ? $responseApi['data']['plan_id'] : null,
         ]);
-        isset($plano->id) ?
-            $response = [
-                'codRetorno' => 200,
-                'message' => $this->codes[200]
-            ] :  $response = [
-            'codRetorno' => 500,
-            'message' => $this->codes[500]
-        ];
+
+        $response = isset($plano->id) ?
+            [
+                'codRetorno' => HttpCodesEnum::OK->value,
+                'message' => HttpCodesEnum::OK->description(),
+            ] : [
+                'codRetorno' => HttpCodesEnum::InternalServerError->value,
+                'message' => HttpCodesEnum::InternalServerError->description(),
+            ];
+
         return response()->json($response);
     }
-
-
-
 
     public function getPlano(Request $request): JsonResponse
     {
         $plano = Planos::find($request->idPlano);
-        isset($plano->id) ?
-            $response = [
-                'codRetorno' => 200,
-                'message' => $this->codes[200],
-                'data' => $plano
-            ] :  $response = [
-                'codRetorno' => 404,
-                'message' => $this->codes[404]
+
+        $response = isset($plano->id) ?
+            [
+                'codRetorno' => HttpCodesEnum::OK->value,
+                'message' => HttpCodesEnum::OK->description(),
+                'data' => $plano,
+            ] : [
+                'codRetorno' => HttpCodesEnum::NotFound->value,
+                'message' => HttpCodesEnum::NotFound->description(),
             ];
+
         return response()->json($response);
     }
 
     public function atualizarDados(Request $request): JsonResponse
     {
-        $campos = ['nome', 'descricao', 'valor', 'quantidadeTags',];
-
+        $campos = ['nome', 'descricao', 'valor', 'quantidadeTags'];
         $campos = Helper::validarRequest($request, $campos);
 
         if ($campos !== true) {
             $response = [
-                'codRetorno' => 400,
-                'message' => $this->codes[-9],
-                'campos' => $campos
+                'codRetorno' => HttpCodesEnum::BadRequest->value,
+                'message' => HttpCodesEnum::MissingRequiredFields->description(),
+                'campos' => $campos,
             ];
             return response()->json($response);
         }
+
         $plano = Planos::findOrFail($request->idPlano);
+
         if (isset($plano->id)) {
             $plano->nome = $request->nome;
             $plano->descricao = $request->descricao;
             $plano->valor = $request->valor;
             $plano->quantidadeTags = $request->quantidadeTags;
             $plano->save();
+
             $response = [
-                'codRetorno' => 200,
-                'message' => $this->codes[200]
+                'codRetorno' => HttpCodesEnum::OK->value,
+                'message' => HttpCodesEnum::OK->description(),
             ];
         } else {
             $response = [
-                'codRetorno' => 500,
-                'message' => $this->codes[500]
+                'codRetorno' => HttpCodesEnum::InternalServerError->value,
+                'message' => HttpCodesEnum::InternalServerError->description(),
             ];
         }
 
@@ -132,63 +132,67 @@ class PlanoController extends Controller
     public function atualizarStatus(Request $request): JsonResponse
     {
         $campos = ['idPlano', 'status'];
-
         $campos = Helper::validarRequest($request, $campos);
 
         if ($campos !== true) {
             $response = [
-                'codRetorno' => 400,
-                'message' => $this->codes[-9],
-                'campos' => $campos
+                'codRetorno' => HttpCodesEnum::BadRequest->value,
+                'message' => HttpCodesEnum::MissingRequiredFields->description(),
+                'campos' => $campos,
             ];
             return response()->json($response);
         }
 
         $plano = Planos::findOrFail($request->idPlano);
+
         if (isset($plano->id)) {
             $plano->status = $request->status;
             $plano->save();
+
             $response = [
-                'codRetorno' => 200,
-                'message' => $this->codes[200]
+                'codRetorno' => HttpCodesEnum::OK->value,
+                'message' => HttpCodesEnum::OK->description(),
             ];
         } else {
             $response = [
-                'codRetorno' => 500,
-                'message' => $this->codes[500]
+                'codRetorno' => HttpCodesEnum::InternalServerError->value,
+                'message' => HttpCodesEnum::InternalServerError->description(),
             ];
         }
+
         return response()->json($response);
     }
 
     public function adicionarFuncionalidades(Request $request): JsonResponse
     {
         $campos = ['idPlano', 'funcionalidades'];
-
         $campos = Helper::validarRequest($request, $campos);
 
         if ($campos !== true) {
             $response = [
-                'codRetorno' => 400,
-                'message' => $this->codes[-9],
-                'campos' => $campos
+                'codRetorno' => HttpCodesEnum::BadRequest->value,
+                'message' => HttpCodesEnum::MissingRequiredFields->description(),
+                'campos' => $campos,
             ];
             return response()->json($response);
         }
 
         $plano = Planos::findOrFail($request->idPlano);
+
         if (isset($plano->id)) {
             $plano->funcionalidades()->sync($request->funcionalidades);
+
             $response = [
-                'codRetorno' => 200,
-                'message' => $this->codes[200]
+                'codRetorno' => HttpCodesEnum::OK->value,
+                'message' => HttpCodesEnum::OK->description(),
             ];
         } else {
             $response = [
-                'codRetorno' => 500,
-                'message' => $this->codes[500]
+                'codRetorno' => HttpCodesEnum::InternalServerError->value,
+                'message' => HttpCodesEnum::InternalServerError->description(),
             ];
         }
+
         return response()->json($response);
     }
 }
