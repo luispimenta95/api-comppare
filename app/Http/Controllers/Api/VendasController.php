fcafda4787c6eee1560fc09646bed886b9828f4d
Seu nome <luisfelipearaujopimenta@gmail.com>
2025-04-19 15:55:17 -0300

    token

--- app/Http/Controllers/Api/VendasController.php
+++ app/Http/Controllers/Api/VendasController.php
@@ -54,7 +54,6 @@ class VendasController extends Controller
         $usuario = Usuarios::find($request->usuario);
         $plano = Planos::find($request->plano);
 
-        dd($plano);
         $data = [
             "cardToken" => $request->token,
             "idPlano" => $plano->idHost,
