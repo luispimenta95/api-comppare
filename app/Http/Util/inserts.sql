ab2d40cf9a4c99573c98d7a5f068a2cb1c3de243
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-05-07 22:05:29 -0300

    Ajuste insert

--- app/Http/Util/inserts.sql
+++ app/Http/Util/inserts.sql
@@ -17,3 +17,18 @@ INSERT INTO `perfil` (`id`, `nome_perfil`, `created_at`, `updated_at`) VALUES
 
 
 update  planos set planos.exibicao = 0 where planos.id in (2,7);
+
+INSERT INTO `questions` VALUES
+                            (1,'O que são os álbuns e subálbuns?','Álbum: É a pasta principal onde você irá organizar os subálbuns.Subálbum: É a pasta secundária onde serão armazenadas as fotos de um tema específico do álbum. Exemplo: Álbum: casa | Subálbuns: sala, quarto, banheiro, área externa, piscina etc','2025-04-30 20:08:16','2025-04-30 20:08:16'),
+                            (2,'O que são as categorias?','As categorias são campos para você inserir informações referente a foto selecionada. Os campos são livres para atender a sua necessidade. Exemplos: Data, peso, altura, medida, profundidade, porcentagem, quantidade etc','2025-04-30 20:12:18','2025-04-30 20:12:18'),
+                            (3,'Quais são os planos existentes?','Hoje, os planos existentes são: gratuito, básico e avançado.','2025-04-30 20:13:10','2025-04-30 20:13:10'),
+                            (4,'Qual a diferença do mensal para o anual?','A diferença entre os planos, é que no plano anual, você possui um desconto de 20% no valor total.','2025-04-30 20:16:48','2025-04-30 20:16:48'),
+                            (5,'Existe taxa de cancelamento?','Não existe taxa de cancelamento. Em caso de cancelamento, não há devolução de valores pagos anteriormente ou do período em curso.','2025-04-30 20:17:24','2025-04-30 20:17:24'),
+                            (6,'O que acontece ao fim dos 07 dias gratuitos?','O valor será cobrado na modalidade de pagamento e plano escolhido anteriormente.','2025-04-30 20:17:54','2025-04-30 20:17:54'),
+                            (7,'Quais as formas de pagamento?','Cartão de crédito e pix recorrente.','2025-04-30 20:18:24','2025-04-30 20:18:24'),
+                            (8,'Existe aplicativo ou apenas a versão web?','Apenas a versão web no momento.','2025-04-30 20:19:44','2025-04-30 20:19:44'),
+                            (9,'Consigo salvar e/ou compartilhar as imagens comparadas?','Sim. Após realizar a comparação de imagens, ficarão disponíveis os botões “salvar” e “compartilhar”.','2025-04-30 20:20:11','2025-04-30 20:20:11'),
+                            (10,'Como posso entrar em contato?','Através do e-mail: contato@comppare.com.br ou das nossas redes sociais: Instagram: comppare.br | Linkedin: linkedin.com/comppare','2025-04-30 20:20:56','2025-04-30 20:20:56'),
+                            (11,'O que são os dados de uso?','É um resumo de uso do usuário dentro da plataforma, mostrando dados como número de álbuns, subábuns, imagens entre outros.','2025-04-30 20:21:13','2025-04-30 20:21:13'),
+                            (12,'Quais as diferenças entre os planos?','- Gratuito: \n01 pasta*\n03 subpastas*\n03 tags*\nCom anúncios\nCompartilhamento em redes sociais\n\n- Básico:\n10 pastas*\n06 subpastas*\n06 tags*\nSem anúncios\nCompartilhamento em redes sociais\nGameficação\nDados de uso\n\n- Avançado: \n20 pastas*\n10 subpastas*\n10 tags*\nSem anúncios\nCompartilhamento em redes sociais\nGameficação\nDados de uso\nDivulgação nas redes sociais da empresa','2025-05-03 20:19:24','2025-05-03 20:19:24'),
+                            (13,'O que é o ranking? Como funcionam as pontuações?','É a classificação dos usuários da plataforma de acordo com a tabela de pontuação abaixo:\n- Criação de álbum ou subálbum: 1 pto\n- Anexou foto com tag/categoria: 2 ptos\n- Usou o botão “comppare”: 2 ptos\n- Baixou imagem: 5 ptos\n- Compartilhou imagem com redes sociais: 20 ptos\n- bônus: resposta de forms de sugestões/melhorias em suporte: 5 ptos','2025-05-03 20:22:20','2025-05-03 20:22:20');
