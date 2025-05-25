903a678785a40dba677546e24dc7ce393aea5000
Seu nome <luisfelipearaujopimenta@gmail.com>
2025-05-25 15:16:12 -0300

    Ajustes rotas

--- routes/api/usuarios.php
+++ routes/api/usuarios.php
@@ -8,12 +8,12 @@ Route::prefix('usuarios')->group(
 
         Route::post('/autenticar', [UsuarioController::class, 'autenticar']);
         Route::post('/cadastrar', [UsuarioController::class, 'cadastrarUsuario']);
-        Route::get('/listar', [UsuarioController::class, 'index']);
         Route::post('/recuperar', [UsuarioController::class, 'getUser']);
         Route::post('/atualizar-status', [UsuarioController::class, 'atualizarStatus']);
         Route::post('/valida-existencia-usuario', [UsuarioController::class, 'validaExistenciaUsuario']);
 
         Route::middleware(['jwt.auth'])->group(function () {
+            Route::get('/listar', [UsuarioController::class, 'index']);
             Route::post('/atualizar-senha', [UsuarioController::class, 'atualizarSenha']);
             Route::post('/atualizar-plano', [UsuarioController::class, 'atualizarPlanoUsuario']);
             Route::post('/atualizar-dados', [UsuarioController::class, 'atualizarDados']);
