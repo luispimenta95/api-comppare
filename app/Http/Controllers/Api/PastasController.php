12b7486382c973edab906ae75e9a6616e9cef128
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-07-01 17:21:42 -0300

    adding workflow forgot

--- app/Http/Controllers/Api/PastasController.php
+++ app/Http/Controllers/Api/PastasController.php
@@ -29,6 +29,7 @@ class PastasController extends Controller
      */
     public function create(Request $request): JsonResponse
     {
+        dd($request->all());
 
         $request->validate([
             'idUsuario' => 'required|exists:usuarios,id', // Validar se o idUsuario existe
