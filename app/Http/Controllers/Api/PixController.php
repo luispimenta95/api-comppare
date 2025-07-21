c6f46b2061f6b4b2f1fe96b7da6bc9321f3222bd
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-07-21 19:50:53 -0300

    pix

--- app/Http/Controllers/Api/PixController.php
+++ app/Http/Controllers/Api/PixController.php
@@ -24,34 +24,16 @@ class PixController extends Controller
             : storage_path('app/certificates/prd.pem');
     }
 
-    public function criarCobranca(): array
+  public function criarCobrancaPixSimples(array $payload, bool $homolog = false): array
 {
-    $homolog = false;
-    $payload = [
-        'vinculo' => [
-            'contrato' => '63100862',
-            'devedor' => [
-                'cpf' => '45164632481',
-                'nome' => 'Fulano de Tal'
-            ],
-            'objeto' => 'Serviço de Streamming de Música.',
-            'dataFinal' => '2025-04-01',
-            'dataInicial' => '2024-04-01',
-            'periodicidade' => 'MENSAL',
-            'valor' => 35.00,
-            'politicaRetentativa' => 'NAO_PERMITE',
-            'loc' => 108,
-            'txid' => '33beb661beda44a8928fef47dbeb2dc5'
-        ]
-    ];
-    $urlBase = $homolog
-        ? 'https://pix-h.api.efipay.com.br/v2/rec/'
-        : 'https://pix.api.efipay.com.br/v2/rec/';
+    $url = $homolog
+        ? 'https://pix-h.api.efipay.com.br/v2/cob/'
+        : 'https://pix.api.efipay.com.br/v2/cob/';
 
     $curl = curl_init();
 
     curl_setopt_array($curl, [
-        CURLOPT_URL => $urlBase,
+        CURLOPT_URL => $url,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -61,10 +43,10 @@ class PixController extends Controller
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => json_encode($payload),
         CURLOPT_SSLCERT => $this->certificadoPath,
-        CURLOPT_SSLCERTPASSWD => '', // Se houver senha no .pem, coloque aqui
+        CURLOPT_SSLCERTPASSWD => '',
         CURLOPT_HTTPHEADER => [
-            "Authorization: Bearer {$this->apiEfi->getToken()}",
-            "Content-Type: application/json"
+            'Authorization: Bearer ' . $this->apiEfi->getToken(),
+            'Content-Type: application/json'
         ],
     ]);
 
@@ -74,9 +56,73 @@ class PixController extends Controller
     curl_close($curl);
 
     if ($erro) {
-        throw new \Exception("Erro cURL: " . $erro);
+        throw new \Exception("Erro ao criar cobrança: $erro");
     }
 
     return json_decode($response, true);
 }
+
+public function consultarQrCodePorLocId(int $idLoc, bool $homolog = false): array
+{
+    $url = $homolog
+        ? "https://pix-h.api.efipay.com.br/v2/loc/{$idLoc}/qrcode"
+        : "https://pix.api.efipay.com.br/v2/loc/{$idLoc}/qrcode";
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_SSLCERT => $this->certificadoPath,
+        CURLOPT_SSLCERTPASSWD => '',
+        CURLOPT_HTTPHEADER => [
+            'Authorization: Bearer ' . $this->apiEfi->getToken()
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    $erro = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($erro) {
+        throw new \Exception("Erro ao consultar QR Code: $erro");
+    }
+
+    return json_decode($response, true);
+}
+public function criarCobranca(){
+    $payload = [
+    "calendario" => [ "expiracao" => 3600 ],
+    "devedor" => [
+        "cpf" => "12345678909",
+        "nome" => "Francisco da Silva"
+    ],
+    "valor" => [ "original" => "0.45" ],
+    "chave" => "chave.pix@email.com.br"
+];
+
+try {
+    $res = $this->criarCobrancaPixSimples($payload, true);
+
+    echo '<pre>' . json_encode($res, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . '</pre>';
+
+    if (isset($res['loc']['id'])) {
+        $qrcode = $this->consultarQrCodePorLocId($res['loc']['id'], true);
+
+        echo 'QR Code Copia e Cola:<br><pre>' . $qrcode['qrcode'] . '</pre>';
+        echo 'Imagem:<br><img src="' . $qrcode['imagemQrcode'] . '" />';
+    }
+
+} catch (\Exception $e) {
+    echo "Erro: " . $e->getMessage();
+}
+}
+
 }
