76764f457b2f49286351c9745a7a827b9eccef14
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-08-05 17:46:02 -0300

    Ajuste tela admin

--- app/Http/Controllers/Api/VendasController.php
+++ app/Http/Controllers/Api/VendasController.php
@@ -40,6 +40,7 @@ class VendasController extends Controller
 
         $campos = ['usuario', 'plano', 'token'];
         $campos = Helper::validarRequest($request, $campos);
+        dd($request);
 
 
         if ($campos !== true) {
