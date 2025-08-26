4ba265aa9918101852d8f50413efdbbc7ae7b795
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-08-25 23:35:12 -0300

    centralizar tipos de pagamento

--- routes/api/pix.php
+++ routes/api/pix.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\Api\PixController;
+use App\Http\Controllers\Api\VendasController;
 use Illuminate\Support\Facades\Route;
 
 // Rotas para PIX
 Route::prefix('pix')->group(function () {
-    Route::post('/enviar', [PixController::class, 'criarCobranca']);
-    Route::get('/cadastrar', [PixController::class, 'registrarWebhook']);
-    Route::get('/ver-webhook', [PixController::class, 'consultarWebhookRecorrente']);
-    Route::post('/atualizar', [PixController::class, 'atualizarCobranca']);
+    Route::post('/enviar', [VendasController::class, 'criarCobranca']);
+    Route::get('/cadastrar', [VendasController::class, 'registrarWebhook']);
+    Route::get('/ver-webhook', [VendasController::class, 'consultarWebhookRecorrente']);
+    Route::post('/atualizar', [VendasController::class, 'atualizarCobrancaPix']);
 });
