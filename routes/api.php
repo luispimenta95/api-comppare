a166726545ee5c4e2f18842b4603a5f54322b3f5
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2025-08-25 23:36:35 -0300

    centralizar tipos de pagamento

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,6 @@ Route::get('/test', function () {
     return response()->json(['message' => 'API funcional na versão: ' . env('APP_VERSION')]);
 });
 
-// Webhook PIX direto (endpoint principal para configuração na EFI)
-Route::post('/pix', [PixController::class, 'webhookSimple'])->middleware('tls.mutual');
-
-Route::get('/webhookcobr', [PixController::class, 'configurarWebhook']);
-
 
 
 Route::get('/planos/listar', [PlanoController::class, 'index']);
